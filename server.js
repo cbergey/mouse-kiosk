5b9bd8a4423d1a003c9eb0a5e43277f0cce74f0d
cbergey <clairebergey@gmail.com>
2026-02-18 17:31:11 -0600

    fix password

--- server.js
+++ server.js
@@ -16,6 +16,18 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"))); // serve HTML/JS/CSS
 
 
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
+// Endpoint to check admin password
+app.post("/api/check-admin", (req, res) => {
+  const { password } = req.body;
+  if (password === ADMIN_PASSWORD) {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false, message: "Incorrect password" });
+  }
+});
+
 // ----------------------
 // Database Setup
 // ----------------------
